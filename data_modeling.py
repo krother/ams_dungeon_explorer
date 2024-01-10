7f9bcdcce73e4a4044fb4a40a25b13118d74f9bd
Kristian Rother <krother@academis.eu>
2024-01-10 12:15:20 +0100

    Update data_modeling.py

--- data_modeling.py
+++ data_modeling.py
@@ -1,8 +1,13 @@
-
+"""
+Implement requirement: walls that are impassable
+"""
+import numpy as np
+import cv2
 from pydantic import BaseModel
 
-
-# define what a wall is
+#
+# define data model
+#
 class Wall(BaseModel):
     x: int
     y: int
@@ -20,8 +25,9 @@ class Player(BaseModel):
     health: int = 100
     coins: int = 0
 
-
-# everything for one level
+#
+# create a level
+#
 player = Player(x=4, y=4)
 walls = [
     Wall(x=0, y=0),
@@ -35,6 +41,72 @@ coins = [
     Coin(x=2, y=5),
 ]
 
-print(player)
-print(walls)
-print(coins)
+#
+# constants measured in pixel
+#
+SCREEN_SIZE_X, SCREEN_SIZE_Y = 640, 640
+TILE_SIZE = 64
+
+
+def draw_dungeon(player, walls, coins, player_img, wall_img, coin_img):
+    frame = np.zeros((SCREEN_SIZE_Y, SCREEN_SIZE_X, 3), np.uint8)
+
+    # TODO: we should organize the image drawing in a smarter way
+    # objects = [player] + walls + coins  # list concatenation
+
+    # draw player
+    xpos, ypos = player.x * TILE_SIZE, player.y * TILE_SIZE
+    frame[ypos:ypos + TILE_SIZE, xpos:xpos + TILE_SIZE] = player_img
+
+    # draw walls
+    for wall in walls:
+        xpos, ypos = wall.x * TILE_SIZE, wall.y * TILE_SIZE
+        frame[ypos:ypos + TILE_SIZE, xpos:xpos + TILE_SIZE] = wall_img
+
+    # draw coins
+    for coin in coins:
+        xpos, ypos = coin.x * TILE_SIZE, coin.y * TILE_SIZE
+        frame[ypos:ypos + TILE_SIZE, xpos:xpos + TILE_SIZE] = coin_img
+
+    cv2.imshow('Dungeon Explorer', frame)
+
+
+# define a function that doubles image size
+size2x = lambda a: np.kron(a, np.ones((2, 2, 1), dtype=a.dtype))
+
+# load image and extract square tiles from it
+wall_img = size2x(cv2.imread('tiles/wall.png'))
+coin_img = size2x(cv2.imread('tiles/gold.png'))
+player_img = size2x(cv2.imread('tiles/deep_elf_high_priest.png'))
+
+
+# define boundaries of the 2D grid
+min_x, max_x = 0, SCREEN_SIZE_X // TILE_SIZE
+min_y, max_y = 0, SCREEN_SIZE_Y // TILE_SIZE
+
+
+exit_pressed = False
+while not exit_pressed:
+
+    draw_dungeon(player, walls, coins, player_img, wall_img, coin_img)
+
+    # handle keyboard input
+    key = chr(cv2.waitKey(1) & 0xFF)
+    if key == 'd' and player.x < 9:
+        player.x += 1
+    elif key == 'a' and player.x > 0:
+        player.x -= 1
+    elif key == 'w' and player.y > 0:
+        player.y -= 1
+    elif key == 's':
+        if player.y < 9:
+            player.y += 1
+        else:
+            player.y = 0  # wrap-around move
+
+    elif key == 'j':
+        player.x += 2
+    elif key == 'q':  # TODO: use ESCAPE instead
+        exit_pressed = True
+    
+cv2.destroyAllWindows()
